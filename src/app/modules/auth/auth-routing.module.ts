3a98b3cdf4c9613063d0da938f211370e7a2bdc4
Bambi <bawe97@yahoo.com>
2021-04-22 11:53:20 +0100

    new commits

---
+++ src/app/modules/auth/auth-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routesApp: Routes = [];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    imports: [
+    RouterModule.forChild(routesApp)],
+    CommonModule
+  ]
+})
+export class AuthRoutingModule { }
