305b7c803a047867f3e94ce4b195de01700ede0f
TorezC <victorytorez@gmail.com>
2021-05-27 11:07:46 -0700

    new update on alignment

--- src/app/modules/pages/landing/services/services.component.ts
+++ src/app/modules/pages/landing/services/services.component.ts
@@ -21,21 +21,26 @@ We ensure that your website puts you on top of your competitors.`
   generalChangeText(type: string) {
     switch (type) {
       case 'WEB':
-      
-        this.myText = `t-start" src="./assets/web dev.png" alt="Web Dev">
-        this the web`;
+        this.myText = `
+        With us, you get a professionally
+        designed website that can be customized according to your requirements. 
+        We ensure that your website puts you on top of your competitors`;
         break;
         case 'MOBILE':
-          this.myText=`this the mobile`;
+          this.myText=`Our team of programmers and designers are committed to developing the best mobile services,
+           products and applications available in the constantly evolving world of mobile technology.`;
         break;
         case 'CONTENT':
-          this.myText=`this the content`;
+          this.myText=`Stop wasting your time, let us do the writing for you! Our solution will provide you with unique textual content. 
+          Not duplicated or plagiarized. Completely and entirely unique.`;
         break;
         case 'MOBMARK':
-          this.myText=`this the mobile marketing`;
+          this.myText=`Our promotional activity is designed for delivery to cell phones,
+           smart phones and other handheld devices, usually as a component of a multi-channel campaign.`;
         break;
         case 'INTERNET':
-          this.myText=`this the internet`;
+          this.myText=`We have advertising and marketing channels that use the Web and email to drive direct sales via electronic commerce, 
+          in addition to sales leads from Web sites or emails.`;
         break;
         default:
           this.myText=`With us, you get a professionally
